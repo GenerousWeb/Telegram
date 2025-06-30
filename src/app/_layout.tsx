37ae5ada7321201ba5f0ed382863561c9eb7983c
rajath.shanbhag <rajath.shanbhag@tafensw.edu.au>
2025-06-30 23:04:17 +1000

    feat: add expo router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot } from "expo-router";
+
+// Define provides shared between components
+export default function RootLayout() {
+    return <Slot/>
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { Redirect } from "expo-router";
+import { StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <Redirect href="/(home)/(tabs)"/>
+        // <View style={styles.container}>
+        //     <Text>Hello World</Text>
+        //     <StatusBar style="auto"/>
+        // </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
